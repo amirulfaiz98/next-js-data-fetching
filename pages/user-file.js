b36b7f883d0a51d5bf8362c4e3edb1e88359cf9d
Amirul Faiz <faiz@baituljannah.com>
2024-02-08 15:42:04 +0800

    use getServerSideProps for Server-side rendering

---
+++ pages/user-file.js
@@ -0,0 +1,11 @@
+function UserProfilePage() {
+    return <h1>Max</h1>
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+    return {
+        
+    };
+}
