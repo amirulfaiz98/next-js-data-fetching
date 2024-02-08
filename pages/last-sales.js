f1a98dcdefdbf32757541ef70c482a8909d55299
Amirul Faiz <faiz@baituljannah.com>
2024-02-08 16:14:49 +0800

    Using the "useSWR" NextJS Hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,36 +1,65 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+import useSWR from "swr";
 
 function LastSalesPage() {
-    const [sales, etSales] =  useState();
-    const [isLoading, setIsLoading] = useState(false);
+    const [sales, etSales] = useState();
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/sales.json"
+  );
+
   useEffect(() => {
-    setIsLoading(true);
-    fetch().then((responese) => response.json)
-    .then(data => {
-        const transformedSales = [];
+    const transformedSales = [];
 
-        for (const key in data) {
-            transformedSales.push({id: key, username: data[key].username, volume: data[key].volume});
-        }
+    for (const key in data) {
+      transformedSales.push({
+        id: key,
+        username: data[key].username,
+        volume: data[key].volume,
+      });
+    }
 
-        setSales();
-        setIsLoading(false);
-    }); // put link such as firebase in fetch example: 'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/sales.json'
-  }, []);
+    setSales(transformedSales);
+  }, [data]);
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://nextjs-course-c81cc-default-rtdb.firebaseio.com/sales.json")
+  //       .then((responese) => response.json)
+  //       .then((data) => {
+  //         const transformedSales = [];
 
-  if (isLoading) {
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+
+  //         setSales();
+  //         setIsLoading(false);
+  //       }); // put link such as firebase in fetch example: 'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/sales.json'
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data ||!sales) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-      return <p>No data yet!</p>;
-  }
-
-  return <ul>
-    {sales.map((sale) => <li key={sale.id}>
-      {sale.username} - ${sale.volume}
-    </li>)}
-  </ul>
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default LastSalesPage;
